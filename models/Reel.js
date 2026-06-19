b66751310ddc900e37e2a24f22eea14477e78ee2
TARUNRAJ1517 <kanakbabumammi@gmail.com>
2026-06-19 12:41:43 +0530

    Update Reel.js

--- models/Reel.js
+++ models/Reel.js
@@ -1,1 +1,18 @@
+const mongoose = require("mongoose");
 
+const ReelSchema = new mongoose.Schema({
+  reelUrl: {
+    type: String,
+    required: true
+  },
+  transcript: {
+    type: String,
+    default: ""
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("Reel", ReelSchema);
