0a64f604f881c1d037c35d2c3026a0231a7622e8
TARUNRAJ1517 <kanakbabumammi@gmail.com>
2026-06-19 12:31:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("✅ MongoDB Connected"))
+.catch(err => console.log("❌ MongoDB Error:", err));
 
 app.get("/", (req, res) => {
   res.send("ReelScribe Backend Running 🚀");
