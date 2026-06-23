1abf39a44aef003e09feafac6a79631321ff8fc2
TARUNRAJ1517 <kanakbabumammi@gmail.com>
2026-06-24 00:07:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -30,7 +30,12 @@ app.use(cors({
 app.use(express.json());
 app.use(express.static("public"));
 
-app.use(session({ secret: process.env.SESSION_SECRET, ... }));
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}));
+
 app.use(passport.initialize());
 app.use(passport.session());
 
